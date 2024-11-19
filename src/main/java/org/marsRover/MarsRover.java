d3ecc8405b48c555ff1d03d8b218e8c1c3a9c678
HEDO <hedo@oocl.com>
2024-11-19 15:42:52 +0800

    [MarsRover] feat: turn left and turn right

--- src/main/java/org/marsRover/MarsRover.java
+++ src/main/java/org/marsRover/MarsRover.java
@@ -2,11 +2,52 @@ package org.marsRover;
 
 public class MarsRover {
 
+    private int x;
+
+    private int y;
+
+    private String direction;
+
+    public MarsRover() {
+    }
+
     public String showStatus() {
-        return "0:0:N";
+        return x + ":" + y + ":" + direction;
     }
 
     public String executeCommand(String command) {
-        return "0:0:W";
+        String direction = this.direction;
+        if (command.equals("L")) {
+            direction = "W";
+
+        } else if (command.equals("R")) {
+            direction = "E";
+        }
+        this.direction = direction;
+        return showStatus();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -8,13 +8,18 @@ public class MarsRoverTest {
     @Test
     public void should_initialize_mars_rover() {
         MarsRover rover = new MarsRover();
-        rover.showStatus();
+        rover.setX(0);
+        rover.setY(0);
+        rover.setDirection("N");
         assertEquals("0:0:N", rover.showStatus());
     }
 
     @Test
     public void should_face_west_when_facing_north_and_turn_left() {
         MarsRover rover = new MarsRover();
+        rover.setX(0);
+        rover.setY(0);
+        rover.setDirection("N");
         String currentStatus = rover.executeCommand("L");
         String report = rover.showStatus();
         assertEquals("0:0:W", currentStatus);
